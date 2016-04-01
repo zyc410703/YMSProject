c4d7e3814d8a93ddcdbfb840d72a57d1ae286547
qianfeng <qianfeng@zhangyanchen.local>
2016-04-01 15:05:49 +0800

    B创建一个PersonB类

---
+++ GitDemo/GitDemo/PersonB.h
@@ -0,0 +1,13 @@
+//
+//  PersonB.h
+//  GitDemo
+//
+//  Created by qianfeng on 16/4/1.
+//  Copyright © 2016年 张彦琛. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PersonB : NSObject
+
+@end
---
+++ GitDemo/GitDemo/PersonB.m
@@ -0,0 +1,13 @@
+//
+//  PersonB.m
+//  GitDemo
+//
+//  Created by qianfeng on 16/4/1.
+//  Copyright © 2016年 张彦琛. All rights reserved.
+//
+
+#import "PersonB.h"
+
+@implementation PersonB
+
+@end
